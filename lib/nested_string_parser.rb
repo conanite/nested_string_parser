d3b9cece33560d216f10687e29c93339dc2d1d7a
conanite <conan@conandalton.net>
2017-11-29 07:58:54 +0100

    node: build from Array of Array of String, as if you wanted to build a tree from CSV

--- lib/nested_string_parser.rb
+++ lib/nested_string_parser.rb
@@ -7,14 +7,40 @@ module NestedStringParser
     def initialize     s=nil ; @nesting, @value, @children = (s ? s.gsub(/^( *)[^ ].*/, '\1').length : -1), (s && s.strip), [] ; end
     def add_child?     child ; (child.nesting > self.nesting) ? add_child(child) : parent.add_child?(child)                    ; end
     def add_child      child ; @children << child ; child.parent = self ; child                                                ; end
+    def nest_child     child ; add_child(child).tap { |c| c.nesting = nesting + 1 }                                            ; end
     def nb?              str ; str && str.strip != ""                                                                          ; end
     def accept str=nil, *ss ; nb?(str) ? add_child?(Node.new(str)).accept(*ss) : (ss.size > 0 ? self.accept(*ss) : self)       ; end
     def root                 ; parent ? parent.root : self                                                                     ; end
-    def format        indent ; "#{indent}#{value}\n#{children.map { |c| c.format "#{indent}  " }.join}"                        ; end
+    def format        indent ; %[#{indent}#{value}\n#{children.map { |c| c.format "#{indent}  " }.join}]                       ; end
+    def find            name ; children.detect { |k| k.value == name }                                                         ; end
   end
 
-  def self.parse str ; NestedStringParser::Node.new.accept(*str.split(/\n/)).root ; end
-  module StringExtension ; def parse_nested_string ; NestedStringParser.parse self ; end ; end
+  # rows is an array of arrays
+  # | A | B | C |
+  # | A | B | D |
+  # | A | E | F |
+  # | A | E | G |
+  # | B | H | J |
+  # | B | H | K |
+  # same as
+  # A
+  #   B
+  #     C
+  #     D
+  #   E
+  #     F
+  #     G
+  # B
+  #   H
+  #     J
+  #     K
+  #
+  def self.from_rows rows, root=new_node
+    rows.each { |row| row.inject(root) { |node, name| node.find(name) || node.nest_child(new_node name) } } ; root
+  end
 
-  def self.patch ; ::String.send :include, StringExtension ; end
+  def self.new_node     val=nil ; NestedStringParser::Node.new.tap { |n| n.value = val }        ; end
+  def self.parse            str ; new_node.accept(*str.split(/\n/)).root                        ; end
+  module Extension              ; def parse_nested_string ; NestedStringParser.parse self.to_s  ; end ; end
+  def self.patch klass=::String ; klass.send :include, Extension                                ; end
 end
---
+++ spec/from_rows_spec.rb
@@ -0,0 +1,62 @@
+require 'spec_helper'
+require "nested_string_parser"
+
+RSpec::describe NestedStringParser do
+  it "parses nothing at all" do
+    node = NestedStringParser.from_rows []
+
+    expect(node.value   ).to eq nil
+    expect(node.nesting ).to eq(-1)
+    expect(node.children).to eq []
+  end
+
+  it "parses a single item" do
+    node = NestedStringParser.from_rows [["hello"]]
+
+    expect(node.value        ).to eq nil
+    expect(node.nesting      ).to eq(-1)
+    expect(node.children.size).to eq 1
+
+    child = node.children.first
+    expect(child.value   ).to eq "hello"
+    expect(child.nesting ).to eq 0
+    expect(child.children).to eq []
+  end
+
+  QUARKS_IN_ROWS = [
+                    %w{ quarks G1 up                 },
+                    %w{ quarks G1 down               },
+                    %w{ quarks G2 strange            },
+                    %w{ quarks G2 charmed            },
+                    %w{ quarks G3 top                },
+                    %w{ quarks G3 bottom             },
+                    %w{ leptons G1 electron          },
+                    %w{ leptons G1 electron-neutrino },
+                    %w{ leptons G2 muon              },
+                    %w{ leptons G2 muon-neutrino     },
+                    %w{ leptons G3 tau               },
+                    %w{ leptons G3 tau-neutrino      },
+                   ]
+
+  it "parses a single item" do
+    node = NestedStringParser.from_rows QUARKS_IN_ROWS
+
+    expect(node.value                ).to eq nil
+    expect(node.nesting              ).to eq(-1)
+    expect(node.children.map(&:value)).to eq %w{ quarks leptons }
+
+    quarks = node.children[0]
+    expect(quarks.nesting                ).to eq 0
+    expect(quarks.children.map(&:value)  ).to eq %w{ G1 G2 G3 }
+    expect(quarks.children.map(&:nesting)).to eq [1,1,1]
+    expect(quarks.children.map { |q| q.children.map(&:nesting) }).to eq [[2,2], [2,2], [2,2]]
+    expect(quarks.children.map { |q| q.children.map(&:value)   }).to eq [%w{up down}, %w{strange charmed}, %w{top bottom}]
+
+    leptons = node.children[1]
+    expect(leptons.nesting                ).to eq 0
+    expect(leptons.children.map(&:value)  ).to eq %w{ G1 G2 G3 }
+    expect(leptons.children.map(&:nesting)).to eq [1,1,1]
+    expect(leptons.children.map { |q| q.children.map(&:nesting) }).to eq [[2,2], [2,2], [2,2]]
+    expect(leptons.children.map { |q| q.children.map(&:value)   }).to eq [%w{electron electron-neutrino}, %w{muon muon-neutrino}, %w{tau tau-neutrino}]
+  end
+end
